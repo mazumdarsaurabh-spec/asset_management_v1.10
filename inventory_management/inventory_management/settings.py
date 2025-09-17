86b9be78ef9c0f215febc547bc4f66afe381749e
Saurabh Mazumdar <mazumdarsaurabh@gmail.com>
2025-09-17 12:37:51 +0530

    This is my tenth commit

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -17,7 +17,13 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", "localhost,127.0.0.1"
+    "ALLOWED_HOSTS",
+    "asset-management-v1-4-9.onrender.com,localhost,127.0.0.1"
+).split(",")
+
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS",
+    "https://asset-management-v1-4-9.onrender.com"
 ).split(",")
 
 # Application definition
